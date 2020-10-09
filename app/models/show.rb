3860c607934d521815aec9991828240bced42ab2
Drew Thompson <drewallynthompson@gmail.com>
2020-10-09 01:01:31 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,8 +5,7 @@ class Show < ActiveRecord::Base
   end
   
   def Show::most_popular_show
-    rating = Show.maximum("rating")
-    Show.find_by(rating: rating)
+    Show.find_by(rating: Show.maximum("rating"))
   end
   
   def Show::lowest_rating
@@ -16,5 +15,8 @@ class Show < ActiveRecord::Base
   def Show::least_popular_show
     Show.find_by(rating: Show.minimum("rating"))
   end
-    
+  
+  def Show::ratings_sum
+    Show.sum("ratings")
+  end
 end
